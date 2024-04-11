aa6c6b3e8157bdafc77ebd14c12641811165ffa7
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2024-04-11 22:52:26 +0300

    img layaut

--- src/components/userItem/UserItem.tsx
+++ src/components/userItem/UserItem.tsx
@@ -10,33 +10,66 @@ interface IProps {
   handleDeleteUser: (id: string) => void;
 }
 
-export const UserItem: React.FC<IProps> = ({ user, activeCard, handleActive, handleDeleteUser }) => {
+export const UserItem: React.FC<IProps> = ({
+  user,
+  activeCard,
+  handleActive,
+  handleDeleteUser,
+}) => {
+  console.log(user);
 
   return (
-    <div className={`${s.cardWrap} ${activeCard === user?.login?.md5 ? s.activeCard : ''}`} onClick={() => handleActive(user?.login?.md5 )}>
-      {activeCard === user?.login?.md5 && <ButtonDel handleDeleteUser={handleDeleteUser} user={user}/>}
+    <div
+      className={`${s.cardWrap} ${
+        activeCard === user?.login?.md5 ? s.activeCard : ""
+      }`}
+      onClick={() => handleActive(user?.login?.md5)}
+    >
+      {activeCard === user?.login?.md5 && (
+        <ButtonDel handleDeleteUser={handleDeleteUser} user={user} />
+      )}
       <div className={s.titleBlog}>
-        <div className={s.imgBlog}></div>
+        <div
+          className={`${s.imgBlog} ${
+            activeCard === user?.login?.md5 ? s.imgBlogActive : ""
+          }`}
+        >
+          {user.picture.thumbnail && (
+            <img
+              className={s.avaImg}
+              src={user.picture?.thumbnail}
+              alt="User Avatar"
+            />
+          )}
+        </div>
         <div className={s.titleText}>
-          <p className={`${s.name} ${activeCard === user?.login?.md5 ? s.activeName : ''}`}>{user?.name?.first} {user?.name?.last}</p>
+          <p
+            className={`${s.name} ${
+              activeCard === user?.login?.md5 ? s.activeName : ""
+            }`}
+          >
+            {user?.name?.first} {user?.name?.last}
+          </p>
           <p className={s.email}>{user.email}</p>
         </div>
+      </div>
+      <div className={s.infoBlog}>
+        <div className={s.infoRow}>
+          <span className={s.infoTitle}>Phone No</span>
+          <span className={s.infoValue}>{user?.phone}</span>
         </div>
-        <div className={s.infoBlog}>
-          <div className={s.infoRow}>
-            <span className={s.infoTitle}>Phone No</span>
-            <span className={s.infoValue}>{user?.phone}</span>
-          </div>
-          <div className={s.infoRow}>
-            <span className={s.infoTitle}>Birthday</span>
-            <span className={s.infoValue}>{formatDate(user?.dob?.date)}</span>
-          </div>
-          <div className={s.infoRow}>
-            <span className={s.infoTitle}>Address</span>
-            <span className={s.infoValue}>{user?.location?.city}, {user?.location?.state}, {user?.location?.country}</span>
-          </div>
+        <div className={s.infoRow}>
+          <span className={s.infoTitle}>Birthday</span>
+          <span className={s.infoValue}>{formatDate(user?.dob?.date)}</span>
         </div>
-
+        <div className={s.infoRow}>
+          <span className={s.infoTitle}>Address</span>
+          <span className={s.infoValue}>
+            {user?.location?.city}, {user?.location?.state},{" "}
+            {user?.location?.country}
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
